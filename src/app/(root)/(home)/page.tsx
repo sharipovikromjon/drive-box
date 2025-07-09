0bfbe4ca92caf5ea107526530e08975f2ea17b33
sharipovikromjon <ikromjonsharipov924@gmail.com>
2025-07-09 18:08:22 +0500

    Add authentication pages and integrate Clerk for user management

--- src/app/(root)/(home)/page.tsx
+++ src/app/(root)/(home)/page.tsx
@@ -1,7 +1,12 @@
+import { UserButton } from "@clerk/nextjs";
 import React from "react";
 
 const HomePage = () => {
-  return <div>HomePage</div>;
+  return (
+    <div>
+      <UserButton />
+    </div>
+  );
 };
 
 export default HomePage;
